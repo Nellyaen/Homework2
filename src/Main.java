9d293cc3daddbcd8c29c1a43ecbf874e5f58a334
nellya <nellya.ziganshina@gmail.com>
2022-11-12 22:14:49 +0100

    Homework 2

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //задача 1
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //задача 2
+        dog = dog * 4;
+        cat =  cat * 4;
+        paper = paper * 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // задача 3 не совсем поняла как делать пункт
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println("dog = "+dog+", cat = "+cat+", paper = "+paper);
+
+
+
+
+    }
+}
